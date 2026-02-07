0decfe7f462c2a2ab15ffdec0345c83887d8a458
Vic May <182175140+Asty-cmd@users.noreply.github.com>
2026-02-07 11:23:24 -0800

    Fix the direction of power application

--- katamari_character.gd
+++ katamari_character.gd
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 	
 	input_dir = Input.get_vector("left", "right", "forward", "backward")
 	if input_dir:
-		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		direction = (Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		#print(direction, " got here")
 		apply_imp = true
 	
