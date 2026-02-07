954767fc65353994dda4bf29d9677ca1386d0178
Vic May <182175140+Asty-cmd@users.noreply.github.com>
2026-02-07 11:20:49 -0800

    Fix character movement bug

--- katamari_character.gd
+++ katamari_character.gd
@@ -1,6 +1,8 @@
 extends RigidBody3D
 
 var direction: Vector3 = Vector3.ZERO
+var input_dir: Vector2 = Vector2.ZERO
+var apply_imp: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,13 +11,21 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	input_dir = Input.get_vector("left", "right", "forward", "backward")
+	if input_dir:
+		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		#print(direction, " got here")
+		apply_imp = true
 	
 	
-func _input_event(camera: Camera3D, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+func _unhandled_input(event: InputEvent) -> void:
 	pass
+	
+		
 	
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	apply_central_force(direction)
+	if apply_imp:
+		print(direction)
+		apply_central_force(direction * 10)
+		direction = Vector3.ZERO
